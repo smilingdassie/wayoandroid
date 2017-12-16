b78941543d77c0e71bee632d7690644a4fe800d8
Donavan <Donavanwallis@hotmail.com>
2017-12-16 11:23:04 +0200

    Initial commit

---
+++ app/src/main/java/com/vf/admin/visualfusion/FragmentSecondScreen.java
@@ -0,0 +1,29 @@
+package com.vf.admin.visualfusion;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentSecondScreen extends Fragment {
+
+
+    public FragmentSecondScreen() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second_screen, container, false);
+    }
+
+}
