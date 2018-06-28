c485354927ceceebd54d4201e594a6d169469a78
smilingdassie <smilingdassie@gmail.com>
2018-06-28 15:12:31 +0200

    test

--- VisaulFusion/app/src/main/java/com/dsouchon/wayo/visualfusion/SurveyActivity.java
+++ VisaulFusion/app/src/main/java/com/dsouchon/wayo/visualfusion/SurveyActivity.java
@@ -608,7 +608,7 @@ public class SurveyActivity extends AppCompatActivity {
 
                 boolean is = ss.getIsCommittedToInstallation();
 
-
+                // small change test
                 Spinner spinnerYesNo = (Spinner) findViewById(R.id.spinnerYesNo);
 
 
